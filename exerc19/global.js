4e8fd8e4110479a9e1141c40bff7ae70099ea023
ThaliaLevy <thalialevy@hotmail.com>
2021-12-31 17:36:15 -0300

    atualizando com novos exercicios e o 23 incompleto

--- exerc19/global.js
+++ exerc19/global.js
@@ -11,75 +11,70 @@ class Pessoa {
     }
 }
 
-function loginAdmin() {
+$(document).ready(() => {
     if (sessionStorage.getItem("pessoas") == null) {
-        document.getElementById("email").value = "admin";
-        document.getElementById("password").value = 123;
+        $("#email").val("admin");
+        $("#password").val(123);
     }
-}
+});
 
-function cadastrar() {
+$("#cadastrar").click(() => {
     pessoas.push(new Pessoa(
-        document.getElementById("email-admin").value,
-        document.getElementById("password-admin").value)
+        $("#email-admin").val(),
+        $("#password-admin").val())
     );
     sessionStorage.setItem("pessoas", JSON.stringify(pessoas));
-
-    document.getElementById("email-admin").value = "";
-    document.getElementById("password-admin").value = "";
-    console.log(pessoas);
-}
+});
 
 function deixarInputVisivel() {
     document.getElementById("bloco1").className = "form-group";
     document.getElementById("bloco2").className = "form-group";
-    document.getElementById("email-editar").value = "";
-    document.getElementById("password-editar").value = "";
+    $("#email-editar").val("");
+    $("#password-editar").val("");
 }
 
-function editar() {
+$("#editar").click(() => {
     let aux = document.getElementById("email-admin").value;
     for (let i = 0; i < pessoas.length; i++) {
         if (pessoas[i] != "") {
             if (pessoas[i].email == aux) {
                 deixarInputVisivel();
                 indicePessoa = i;
-                document.getElementById("email-admin").value = "";
-                document.getElementById("password-admin").value = "";
+                $("#email-admin").val("");
+                $("#password-admin").val("");
                 break;
             }
         }
     }
     console.log(pessoas);
-}
+});
 
-function salvarEdicao() {
-    pessoas[indicePessoa].email = document.getElementById("email-editar").value;
-    pessoas[indicePessoa].senha = document.getElementById("password-editar").value;
+$("#salvarEdicao").click(() => {
+    pessoas[indicePessoa].email = $("#email-editar").val();
+    pessoas[indicePessoa].senha = $("#password-editar").val();
     console.log(pessoas);
     document.getElementById("bloco1").className = "form-group none";
     document.getElementById("bloco2").className = "form-group none";
-}
+});
 
-function excluir() {
-    let aux = document.getElementById("email-admin").value;
+$("#excluir").click(() => {
+    let aux = $("#email-admin").val();
     for (let i = 0; i < pessoas.length; i++) {
         if (pessoas[i] != "") {
             if (pessoas[i].email == aux) {
                 pessoas[i] = "";
-                document.getElementById("email-admin").value = "";
+                $("#email-admin").val("");
                 alert("Cadastro excluído!");
                 break;
             }
         }
     }
-    console.log(pessoas);
-}
+});
 
-function guardarPerfil() {
-    let aux = document.getElementById("opcPerfil").value;
+$("#guardarPerfil").click(() => {
+    let aux = $("#opcPerfil").val();
     sessionStorage.setItem("perfil", aux);
-}
+});
 
 function validarConta() {
     let aux = JSON.parse(sessionStorage.getItem("pessoas"));
@@ -115,6 +110,4 @@ function redirecionar() {
             window.location.href = "homeUser.html";
         }
     }
-
-
 }
