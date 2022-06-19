e65c62963079554b914f503deec713222cf4fea8
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-06-18 22:00:23 -0300

    Delete aula19_20 directory

--- aula19_20/Conta.java
+++
@@ -1,6 +0,0 @@
-package aula19_20;
-
-public class Conta {
-	public String nrConta, agencia, titular;
-	public int saldo;
-}
--- aula19_20/Corrente.java
+++
@@ -1,7 +0,0 @@
-package aula19_20;
-
-public class Corrente extends Conta {
-	public String limite;
-	
-	
-}
